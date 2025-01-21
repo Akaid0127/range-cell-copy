9c6c9b979e775ac495a5ae084db44f3bdf0b145b
Akaid0127 <lzy127125@163.com>
2025-01-21 15:18:09 +0800

    获取数据并展示

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css';
 import { Button, message, Table } from 'antd';
 import axios from 'axios';
 
+interface ITableRecord {
+  id: string;
+  name: string;
+  age: string;
+  address: string;
+  describle: string;
+  phone: string;
+  email: string;
+}
+
 const defaultTableColumns = [
   { title: 'Name', dataIndex: 'name', key: 'name', width: 100 },
-  { title: 'Age', dataIndex: 'age', key: 'age', width: 100 },
+  { title: 'Age', dataIndex: 'age', key: 'age', width: 50 },
   { title: 'Address', dataIndex: 'address', key: 'address', width: 100 },
   { title: 'Describe', dataIndex: 'describe', key: 'describe', width: 100 },
   { title: 'Phone', dataIndex: 'phone', key: 'phone', width: 100 },
   { title: 'Email', dataIndex: 'email', key: 'email', width: 100 },
 ];
 
+// 格式化对象数组
+function convertValueToString(arr: any[]) {
+  for (let i = 0; i < arr.length; i++) {
+    const obj = arr[i];
+    for (let key in obj) {
+      if (typeof obj[key] !== 'string') {
+        obj[key] = obj[key].toString();
+      }
+    }
+  }
+  return arr;
+}
+
 const App: React.FC = () => {
+  const [tableRecords, setTableRecords] = useState<ITableRecord[]>([]);
+
   useEffect(() => {}, []);
 
   // 获取数据
@@ -24,6 +49,14 @@ const App: React.FC = () => {
         if (res.status === 200) {
           const resData = res.data.list;
           console.log(resData);
+          setTableRecords(
+            convertValueToString([...resData]).map((item: any) => {
+              return {
+                key: item.id,
+                ...item,
+              };
+            })
+          );
         } else {
           message.error(res.message);
         }
@@ -34,7 +67,9 @@ const App: React.FC = () => {
   };
 
   // 清空数据
-  const handleClearData = () => {};
+  const handleClearData = () => {
+    setTableRecords([]);
+  };
 
   return (
     <>
@@ -47,8 +82,10 @@ const App: React.FC = () => {
           <Table
             bordered
             size="small"
+            dataSource={tableRecords}
             columns={defaultTableColumns}
             scroll={{ x: 800, y: 600 }}
+            pagination={false}
           />
         </div>
       </div>
