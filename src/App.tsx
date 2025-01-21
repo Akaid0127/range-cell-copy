9d147d563fd92b388ea237262eb050c7fb39f524
Akaid0127 <lzy127125@163.com>
2025-01-21 14:23:18 +0800

    页面结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { useState } from 'react';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import { Button, Table } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0);
-  
+const defaultTableColumns = [
+  { title: 'Name', dataIndex: 'name', key: 'name', width: 100 },
+  { title: 'Age', dataIndex: 'age', key: 'age', width: 100 },
+  { title: 'Address', dataIndex: 'address', key: 'address', width: 100 },
+  { title: 'Describe', dataIndex: 'describe', key: 'describe', width: 100 },
+  { title: 'Phone', dataIndex: 'phone', key: 'phone', width: 100 },
+  { title: 'Email', dataIndex: 'email', key: 'email', width: 100 },
+];
+
+const App: React.FC = () => {
+  // 获取数据
+  const handleGetData = () => {};
+
+  // 清空数据
+  const handleClearData = () => {};
 
   return (
     <>
-      <div>123123</div>
+      <div className={styles.rangeCopyWrapper}>
+        <div className={styles.optContent}>
+          <Button onClick={handleGetData}>获取数据</Button>
+          <Button onClick={handleClearData}>清空数据</Button>
+        </div>
+        <div className={styles.tableContent}>
+          <Table
+            bordered
+            size="small"
+            columns={defaultTableColumns}
+            scroll={{ x: 800, y: 600 }}
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
